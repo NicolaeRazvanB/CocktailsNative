f1d167b6577138e9523117c385773813ebae826d
NicolaeRazvanB <nicolassky16@gmail.com>
2023-05-13 15:36:12 +0300

    fixed save button to bd cocktail details

--- Views/CocktailDetailsScreen.jsx
+++ Views/CocktailDetailsScreen.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 // import SQLite from "react-native-sqlite-storage";
-import * as SQLite from 'expo-sqlite';
+import * as SQLite from "expo-sqlite";
 
 export default function CocktailDetailsScreen({ route }) {
   const { cocktail } = route.params;
   const [saved, setSaved] = useState(false);
 
   let ingredients = [];
-  let ingredientsText = '';
-  let measuresText = '';
+  let ingredientsText = "";
+  let measuresText = "";
 
   for (let i = 1; i <= 15; i++) {
     const ingredient = cocktail[`strIngredient${i}`];
@@ -29,48 +29,41 @@ export default function CocktailDetailsScreen({ route }) {
   }
 
   // --- db functionality ---
-  const db = SQLite.openDatabase('savedCocktails.db');
+  const db = SQLite.openDatabase("savedCocktails.db");
   db.transaction((tx) => {
     tx.executeSql(
-      'CREATE TABLE IF NOT EXISTS favorites (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, category TEXT, type TEXT, glass TEXT, instructions TEXT, imageUrl TEXT, ingredients TEXT, measures TEXT);',
+      "CREATE TABLE IF NOT EXISTS favorites (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, category TEXT, type TEXT, glass TEXT, instructions TEXT, imageUrl TEXT, ingredients TEXT, measures TEXT);",
       [],
-      () => console.log('Favorites table created'),
+      () => console.log("Favorites table created"),
       //   console.log(cocktail),
       //   console.log(ingredientsText),
       //   console.log(measuresText),
-      (err) => console.log('Error:', err)
+      (err) => console.log("Error:", err)
     );
   });
 
   const handleSaveToFavorites = () => {
     db.transaction((tx) => {
-      try {
-        console.log(cocktail.strDrink),
-          console.log(cocktail.strCategory),
-          console.log(cocktail.strAlcoholic),
-          console.log(cocktail.strGlass),
-          console.log(cocktail.strInstructions),
-          console.log(cocktail.strDrinkThumb),
-          console.log(ingredientsText),
-          console.log(measuresText),
-          tx.executeSql(
-            'INSERT INTO favorites (name, category, type, glass, instructions, imageUrl, ingredients, measures) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-            [
-              cocktail.strDrink,
-              cocktail.strCategory,
-              cocktail.strAlcoholic,
-              cocktail.strGlass,
-              cocktail.strInstructions,
-              cocktail.strDrinkThumb,
-              ingredientsText,
-              measuresText,
-            ],
-            () => console.log('Cocktail saved to favorites'),
-            (err) => console.log('Error2:', err)
+      tx.executeSql(
+        "INSERT INTO favorites (name, category, type, glass, instructions, imageUrl, ingredients, measures) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          cocktail.strDrink,
+          cocktail.strCategory,
+          cocktail.strAlcoholic,
+          cocktail.strGlass,
+          cocktail.strInstructions,
+          cocktail.strDrinkThumb,
+          ingredientsText,
+          measuresText,
+        ],
+        () => {
+          console.log("Cocktail saved to favorites");
+          tx.executeSql("SELECT * FROM favorites", [], (_, { rows }) =>
+            console.log(rows)
           );
-      } catch (error) {
-        console.log('Error inserting cocktail into favorites:', error);
-      }
+        },
+        (err) => console.log("Error inserting cocktail into favorites:", err)
+      );
     });
   };
 
@@ -96,49 +89,49 @@ export default function CocktailDetailsScreen({ route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'stretch',
-    justifyContent: 'flex-start',
+    flexDirection: "row",
+    alignItems: "stretch",
+    justifyContent: "flex-start",
     padding: 10,
   },
   textContainer: {
     flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
     padding: 10,
   },
   imageContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   subtitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 10,
     marginBottom: 5,
   },
   image: {
-    height: '100%',
-    width: '100%',
-    resizeMode: 'cover',
+    height: "100%",
+    width: "100%",
+    resizeMode: "cover",
   },
   button: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     borderRadius: 5,
     padding: 10,
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
     marginTop: 10,
   },
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 16,
   },
 });
